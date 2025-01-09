24d14726219fa40a50531040b91c9d87a54b8f17
Oksana Kolesova <kolesovaok85@gmail.com>
2025-01-09 20:52:11 +0200

    upgr app

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
 import "./index.css";
 import "modern-normalize";
 import App from "./App.jsx";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
